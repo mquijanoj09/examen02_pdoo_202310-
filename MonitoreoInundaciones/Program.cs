e916a6ec1844b44fce41432aa7a5e8557a57a76f
mquijanoj09 <mquijanoj@hotmail.com>
2023-03-17 00:04:12 -0500

    Solicitud datos

--- MonitoreoInundaciones/Program.cs
+++ MonitoreoInundaciones/Program.cs
@@ -1,3 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace MonitoreoInundaciones
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para evaluar el riesgo de inundaciones en diferentes zonas");
+
+            int cantidadZonas = 0;
+            bool cantidadZonasCorrecta = false;
+
+            do
+            {
+                try
+                {
+                    Console.Write("\nIngrese la cantidad de zonas: \n");
+                    cantidadZonas = int.Parse(Console.ReadLine()!);
+
+                    if (cantidadZonas <= 0)
+                        Console.WriteLine("El dato ingresado debe ser entero positivo. Intenta nuevamente.");
+                    else
+                        cantidadZonasCorrecta = true;
+                }
+                catch (FormatException elError)
+                {
+                    Console.WriteLine("El dato ingresado no representa una cantidad. Intenta nuevamente.");
+                    Console.WriteLine(elError.Message);
+                }
+            }
+            while (cantidadZonasCorrecta == false);
+
+
+        }
+    }
+}
